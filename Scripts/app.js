5ef3e7ad0a8a218ada28ed80802f0687bf116c12
Ananthan <ananthan@hotmail.ca>
2021-05-14 00:11:57 -0400

    initial commit

---
+++ Scripts/app.js
@@ -0,0 +1,14 @@
+"use strict";
+
+(function(){
+
+    function Start()
+    {
+        let myVariable = 20;
+        console.log("my name is: " + myVariable);
+    }
+}
+
+window.addEventListener("load",Start);
+    
+})()
